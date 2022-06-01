a4813cbe509ce1990d33dc5f76d63fc43fa6a27c
Md. Naimul Hasan <naim886@gmail.com>
2022-06-01 23:39:00 +0600

    fix

--- src/components/Home/include/HomeItemCard.js
+++ src/components/Home/include/HomeItemCard.js
@@ -1,16 +1,152 @@
 import React from 'react';
+import Slider from "react-slick";
 import photo from './../../../assets/images/photographer-407068_1920.jpg'
+import icon1 from './../../../assets/images/icons/1.jpg'
+import icon2 from './../../../assets/images/icons/2.jpg'
+import icon3 from './../../../assets/images/icons/3.jpg'
+import icon4 from './../../../assets/images/icons/4.jpg'
+import icon5 from './../../../assets/images/icons/5.jpg'
+import icon6 from './../../../assets/images/icons/6.jpg'
+import icon7 from './../../../assets/images/icons/7.jpg'
+import icon8 from './../../../assets/images/icons/8.jpg'
+import icon9 from './../../../assets/images/icons/9.jpg'
+import icon10 from './../../../assets/images/icons/10.jpg'
+import icon11 from './../../../assets/images/icons/11.jpg'
+import icon12 from './../../../assets/images/icons/12.jpg'
+import icon13 from './../../../assets/images/icons/13.jpg'
+import icon14 from './../../../assets/images/icons/14.jpg'
+import icon15 from './../../../assets/images/icons/15.jpg'
+import icon16 from './../../../assets/images/icons/16.jpg'
+import icon17 from './../../../assets/images/icons/17.jpg'
+import icon18 from './../../../assets/images/icons/18.jpg'
+import icon19 from './../../../assets/images/icons/19.jpg'
+import icon20 from './../../../assets/images/icons/20.jpg'
+import icon21 from './../../../assets/images/icons/21.jpg'
+import icon22 from './../../../assets/images/icons/22.jpg'
+
 
 const HomeItemCard = () => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 9,
+        slidesToScroll: 9
+    };
+
     return (
         <div className={'container section-gap'}>
             <div className="row">
                 <div className="col-md-12 my-5">
                     <h1 className={'text-center text-uppercase text-theme'}>Find your best match</h1>
                 </div>
+            </div>
+            <div className="row">
+                <div className="col-md-11">
+                    <div className="category-area">
+                        <Slider {...settings}>
+                            <a href={'/'} className="category-item">
+                                <img src={icon1}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon2}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon3}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon4}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon5}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon6}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon7}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon8}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon9}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon10}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon11}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon12}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon13}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon14}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon15}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon16}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon17}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon18}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon19}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon20}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon21}/>
+                                <p>Category 1</p>
+                            </a>
+                            <a href={'/'}  className="category-item">
+                                <img src={icon22}/>
+                                <p>Category 1</p>
+                            </a>
+                        </Slider>
+                    </div>
+                </div>
+                <div className="col-md-1">
+                    <button>Filter</button>
+                </div>
+            </div>
+
+            <div className="row">
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -24,6 +160,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -37,6 +174,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -50,6 +188,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -63,6 +202,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -76,6 +216,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -89,6 +230,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -102,6 +244,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -115,6 +258,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -128,6 +272,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -141,6 +286,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
@@ -154,6 +300,7 @@ const HomeItemCard = () => {
                 <div className="col-md-3 mt-4">
                     <div className="home-card-item">
                         <div className="image-container">
+                            <button><i className="fa-solid fa-heart"></i></button>
                             <img src={photo} className={'img-thumbnail'} alt={'photo'}/>
                         </div>
                         <div className="content">
--- src/components/include/Footer.js
+++ src/components/include/Footer.js
@@ -1,101 +1,144 @@
-import React, { useState } from 'react';
-import { Button, Offcanvas } from 'react-bootstrap';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import React, {useState} from 'react';
+import {Button, Offcanvas} from 'react-bootstrap';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCoffee} from "@fortawesome/free-solid-svg-icons";
+import ios from './../../assets/images/ios.png'
+import app from './../../assets/images/app.png'
+
 const Footer = () => {
-  const date = new Date().getFullYear()
+    const date = new Date().getFullYear()
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-  return (
-    <section className="footer-area">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 footer-left">
-            <div className="footer-link">
+    return (
+        <section className="footer-area">
+            <div className="container">
+                <div className="row py-5">
+                    <div className="col-md-4">
+                        <h6>Support</h6>
+                        <ul>
+                            <li>Help Center</li>
+                            <li>AirCover</li>
+                            <li>Safety information</li>
+                            <li>Supporting people with disabilities</li>
+                            <li>Cancellation options</li>
+                            <li>Our COVID-19 Response</li>
+                            <li>Report a neighborhood concern</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4">
+                        <h6>About</h6>
+                        <ul>
+                            <li>Newsroom</li>
+                            <li>Learn about new features</li>
+                            <li>Letter from our founders</li>
+                            <li>Careers</li>
+                            <li>Investors</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4">
+                        <h6>DOWNLOAD APP (ANDROID & IOS)</h6>
+                        <div className="app-area">
+                            <img src={ios} alt={'iOS download'}/>
+                            <img src={app} alt={'google download'}/>
+                        </div>
+
+                        <div className="news-letter  mt-5">
+                            <label>Subscribe for Newsletter</label>
+                            <div className="input-group">
+                                <input type={"text"} className={'form-control form-control-sm'} />
+                                <span className={'input-group-text'}>Subscribe</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 footer-left">
+                        <div className="footer-link">
               <span className="copyright">
                 <small>&copy; {date} khan, Inc.</small>
               </span>
-              <ul>
-                <li>
-                  <a href="#">
-                    <small>Privacy</small>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <small>Terms</small>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <small>Sitemap</small>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <small>Destinations</small>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-md-6 footer-right">
-            <div className="language">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <small>Privacy</small>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <small>Terms</small>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <small>Sitemap</small>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <small>Destinations</small>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-6 footer-right">
+                        <div className="language">
               <span>
                 <small>
-                  <FontAwesomeIcon icon={faCoffee} />
+                  <FontAwesomeIcon icon={faCoffee}/>
                 </small>
               </span>
-              <span>
+                            <span>
                 <small>English(US)</small>
               </span>
-            </div>
-            <div className="currency">
-              <span>$</span>
-              <span>
+                        </div>
+                        <div className="currency">
+                            <span>$</span>
+                            <span>
                 <small>USD</small>
               </span>
-            </div>
-            <div className="support">
-              <div onClick={ handleShow}>
+                        </div>
+                        <div className="support">
+                            <div onClick={handleShow}>
                 <span>
                   <small>Support & resources</small>
                 </span>
-                <span>^</span>
-              </div>
+                                <span>^</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-      {/* <Button onClick={handleShow}>Launch</Button> */}
-      <Offcanvas
-        className="footer-big"
-        show={show}
-        onHide={handleClose}
-        placement="bottom"
-      >
-        <Offcanvas.Header closeButton></Offcanvas.Header>
-      <Offcanvas.Body closeButton>
-          <div className="row">
-            <div className="col-md-3">
-                <h6>Support</h6>
-                <ul>
-                  <li>Help Center</li>
-                  <li>AirCover</li>
-                  <li>Safety information</li>
-                  <li>Supporting people with disabilities</li>
-                  <li>Cancellation options</li>
-                  <li>Our COVID-19 Response</li>
-                  <li>Report a neighborhood concern</li>
-                </ul>
-            </div>
-          </div>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </section>
-  );
+            {/* <Button onClick={handleShow}>Launch</Button> */}
+            <Offcanvas
+                className="footer-big"
+                show={show}
+                onHide={handleClose}
+                placement="bottom"
+            >
+                <Offcanvas.Header closeButton></Offcanvas.Header>
+                <Offcanvas.Body closeButton>
+                    <div className="row">
+                        <div className="col-md-3">
+                            <h6>Support</h6>
+                            <ul>
+                                <li>Help Center</li>
+                                <li>AirCover</li>
+                                <li>Safety information</li>
+                                <li>Supporting people with disabilities</li>
+                                <li>Cancellation options</li>
+                                <li>Our COVID-19 Response</li>
+                                <li>Report a neighborhood concern</li>
+                            </ul>
+                        </div>
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </section>
+    );
 };
 
 export default Footer;
