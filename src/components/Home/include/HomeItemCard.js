80ace18bb8c76f242a9f64f1da2501986770da90
MKASIF <mdasifkhan089@gmail.com>
2022-06-02 02:29:59 +0600

    Add new tag on new product Category filter section design add favrite button

--- src/components/Home/include/HomeItemCard.js
+++ src/components/Home/include/HomeItemCard.js
@@ -35,109 +35,111 @@ const HomeItemCard = () => {
   };
 
   return (
-    <div className={'container section-gap'}>
+    <div className={"container section-gap"}>
       <div className="row">
         <div className="col-md-12 my-5">
-          <h1 className={'text-center text-uppercase text-theme'}>Find your best match</h1>
+          <h1 className={"text-center text-uppercase text-theme"}>
+            Find your best match
+          </h1>
         </div>
       </div>
-      <div className="row">
+      <div className="row align-items-center">
         <div className="col-md-11">
           <div className="category-area">
             <Slider {...settings}>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon1} />
-                <p>Category 1</p>
+                <p>Islands</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon2} />
-                <p>Category 1</p>
+                <p>Arctic</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon3} />
-                <p>Category 1</p>
+                <p>Cabins</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon4} />
-                <p>Category 1</p>
+                <p>Arctic</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon5} />
-                <p>Category 1</p>
+                <p>Cabins</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon6} />
-                <p>Category 1</p>
+                <p>Arctic</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon7} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon8} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon9} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon10} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon11} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon12} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon13} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon14} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon15} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon16} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon17} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon18} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon19} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon20} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon21} />
                 <p>Category 1</p>
               </a>
-              <a href={'/'} className="category-item">
+              <a href={"/"} className="category-item">
                 <img src={icon22} />
                 <p>Category 1</p>
               </a>
             </Slider>
           </div>
         </div>
-        <div className="col-md-1">
-          <button>Filter</button>
+        <div className="col-md-1 filter">
+          <button className="btn-filter">Filter</button>
         </div>
       </div>
 
@@ -145,179 +147,228 @@ const HomeItemCard = () => {
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button className="btn-favorite">
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <span>New</span>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <span>New</span>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <span>New</span>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
         <div className="col-md-3 mt-4">
           <div className="home-card-item">
             <div className="image-container">
-              <button><i className="fa-solid fa-heart"></i></button>
-              <img src={photo} className={'img-thumbnail'} alt={'photo'} />
+              <button>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+              <img src={photo} className={"img-thumbnail"} alt={"photo"} />
             </div>
             <div className="content">
               <h6>Gardone Riviera, Italy</h6>
               <p>Designed by David Chipperfield</p>
               <p>Aug 1 – 7</p>
-              <p><strong>$12,761 USD</strong> Night</p>
+              <p>
+                <strong>$12,761 USD</strong> Night
+              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="col-md-12 my-4 text-center">
-        <button className={'view-more-button mt-4'}>View More</button>
+        <button className={"view-more-button mt-4"}>View More</button>
       </div>
-
     </div>
-  )}
+  );}
 
 
 
